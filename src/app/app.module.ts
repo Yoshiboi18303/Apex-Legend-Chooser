04bc07216a822251c6c0b767443ac46fa4c9c70a
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2022-12-08 22:40:40 -0500

    Add Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LegendChooserComponent } from './legend-chooser/legend-chooser.component';
 import { WeaponComboChooserComponent } from './weapon-combo-chooser/weapon-combo-chooser.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LegendChooserComponent,
-    WeaponComboChooserComponent
+    WeaponComboChooserComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
